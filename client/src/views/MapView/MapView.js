2239707fbfbdb890b3ca0c6ef9162d786ed98790
SabrinaSnider <sabrina.snider10@gmail.com>
2020-02-27 16:08:59 -0500

    Basic google maps implementation

--- client/src/views/MapView/MapView.js
+++ client/src/views/MapView/MapView.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import SearchableMap from '../../components/Map/SearchableMap';
+import GMaps from '../../components/GoogleMaps/GMaps'
 
 function MapView() {
     return (
         <div className="App">
-            <SearchableMap/>
+            <GMaps/>
         </div>
     );
 }
