efc9843c8469bed0cf457f2fa4888220b49b9a5e
SabrinaSnider <sabrina.snider10@gmail.com>
2020-02-29 19:10:33 -0500

    Displaying instructions on the left side of the screen and route on the right side

--- client/src/components/GoogleMaps/GMaps.js
+++ client/src/components/GoogleMaps/GMaps.js
@@ -2,40 +2,48 @@ import React, { useState, useEffect } from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, DirectionsRenderer } from "react-google-maps"
 import MapStyles from './MapStyles'
 
-let origin = [41.8507300, -87.6512600]
-let destination = [41.8525800, -87.6514100]
+// method to grab list of directions from google api
+const getDirections = (origin, destination, directions, setDirections) => {
+    const google = window.google;
+    const DirectionsService = new google.maps.DirectionsService();
 
+    DirectionsService.route({
+        origin: new google.maps.LatLng(origin[0], origin[1]),
+        destination: new google.maps.LatLng(destination[0], destination[1]),
+        travelMode: google.maps.TravelMode.DRIVING,
+    }, (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+            console.log('Directions are:', result)
+            setDirections(result)
+        } else {
+            console.error(`error fetching directions ${result}`);
+            setDirections(null)
+        }
+    });
+}
+
+// maybe add restriction to map to limit panning later
 const MyMap = (props) => {
     const google = window.google;
-    const [directions, setDirections] = useState(null);
 
     useEffect(() => {
         // Functions passed to useEffect are executed on every component rendering
         // If values are passed to the array, useEffect will execute every time those value changes
-        const DirectionsService = new google.maps.DirectionsService();
-        console.log("Props are:", props)
-
-        DirectionsService.route({
-            origin: new google.maps.LatLng(origin[0], origin[1]),
-            destination: new google.maps.LatLng(destination[0], destination[1]),
-            travelMode: google.maps.TravelMode.DRIVING,
-        }, (result, status) => {
-            if (status === google.maps.DirectionsStatus.OK) {
-                console.log('Directions are:', result)
-                setDirections(result)
-            } else {
-                console.error(`error fetching directions ${result}`);
-            }
-        });
+        getDirections(props.origin, props.destination, props.directions, props.setDirections)
     }, [props.origin, props.destination])
 
     return (
         <GoogleMap
             defaultZoom={10}
             defaultCenter={{ lat: -34.397, lng: 150.644 }}
-            defaultOptions = {{styles: MapStyles, fullscreenControl: false, streetViewControl: false, mapTypeControl: false}}
+            defaultOptions = {{
+                styles: MapStyles, 
+                fullscreenControl: false, 
+                streetViewControl: false, 
+                mapTypeControl: false
+            }}
         >
-            {directions && <DirectionsRenderer directions={directions} />}
+            {props.directions && <DirectionsRenderer directions={props.directions} />}
         </GoogleMap>
     )
 }
--- client/src/views/MapView/MapView.js
+++ client/src/views/MapView/MapView.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import SearchableMap from '../../components/Map/SearchableMap';
+import React, { useState, useEffect } from 'react'
 import GMaps from '../../components/GoogleMaps/GMaps'
+import Instructions from '../../components/GoogleMaps/Instructions'
 
 let token = ''
 
-// let origin = [41.8507300, -87.6512600]
-// let destination = [41.8525800, -87.6514100]
+let origin = [41.8507300, -87.6512600]
+let destination = [42, -88]
 
 function MapView() {
+    const [directions, setDirections] = useState(null);
+
     return (
-        <div className="App" style = {{width: `80vw`, height: `80vh`, margin: '0 auto'}}>
+        <div className="App">
+            <div style = {{float: 'left', margin: '40px'}}>
+                <Instructions
+                    directions = {directions}
+                />
+            </div>
+            <div style = {{width: `45vw`, height: `60vh`, float: 'right', margin: '40px'}}>
             <GMaps
                 googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=` + token}
                 loadingElement = {<div style = {{height: '100%'}}/>}
                 containerElement = {<div style = {{height: '100%'}}/>}
                 mapElement = {<div style = {{height: '100%'}}/>}
-                // origin = {origin}
-                // destination = {destination}
+                origin = {origin}
+                destination = {destination}
+                directions = {directions}
+                setDirections = {setDirections}
             />
+            </div>
         </div>
     );
 }
