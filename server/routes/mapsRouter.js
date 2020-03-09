1ff934438a34a034334d11a611ed7d8af68b98f1
SabrinaSnider <sabrina.snider10@gmail.com>
2020-03-09 16:35:12 -0400

    Removed call to config file for remote deployment purposes

--- server/routes/mapsRouter.js
+++ server/routes/mapsRouter.js
@@ -3,9 +3,9 @@ const express = require('express');
 
 const mapsRouter = express.Router();
 
-mapsRouter.get('/token', (req, res) => {
-    console.log("Triggered Get response", config.google.api_key)
-    res.send(config.google.api_key)
-});
+// mapsRouter.get('/token', (req, res) => {
+//     console.log("Triggered Get response", config.google.api_key)
+//     res.send(config.google.api_key)
+// });
 
 module.exports = mapsRouter
