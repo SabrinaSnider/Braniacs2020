7869f01c5f98720047df50102b715a2bb3cc9803
SabrinaSnider <sabrina.snider10@gmail.com>
2020-03-09 16:09:14 -0400

    Started making a login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home";
 import NotFound from "./views/NotFound";
 import NavBar from "./components/Header/NavBar";
+import Login from './views/Login/Login'
 import ParkingDirections from './views/MapView/ParkingDirections'
 import HospitalDirections from './views/MapView/HospitalDirections'
 
@@ -15,6 +16,7 @@ const App = () => {
         <Route exact path="/Home" component={Home} />
         <Route exact path="/ParkingDirections" component={ParkingDirections} /> 
         <Route exact path="/HospitalDirections" component={HospitalDirections} /> 
+        <Route exact path="/Login" component={Login} /> 
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
