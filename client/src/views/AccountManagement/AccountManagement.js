92e6f874e548fd54464dbc457dc9c26673c0cea8
SabrinaSnider <sabrina.snider10@gmail.com>
2020-03-28 23:44:36 -0400

    reformatted page in a card layout

--- client/src/views/AccountManagement/AccountManagement.js
+++ client/src/views/AccountManagement/AccountManagement.js
@@ -1,51 +1,43 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './AccountManagement.css'
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Form, Button, Card } from 'react-bootstrap'
 
 /*
     Account management page for viewing and displaying user information.
-    Displays email, password (hidden), date of birth, first name, last name.
+    Displays email, date of birth, first name, last name.
 */
 function AccountManagement(props) {
     return (
-        <div id="account-management-page">
+        <Card id="account-management-page">
             <h1 id="account-management-header" className="account-row">Account Informaton</h1>
-            
+
             <Form>
-                <Form.Group as={Row} className="account-row">
-                    <Form.Label className="account-label">Name</Form.Label>
-                    <Col>
-                        <Form.Control/>
-                    </Col>
+                <Form.Group className="account-row">
+                    <h2 className="account-label">First name</h2>
+                    <Form.Control defaultValue="name1"/>
                 </Form.Group>
 
-                <Form.Group as={Row} className="account-row">
-                    <Form.Label className="account-label">Date of Birth</Form.Label>
-                    <Col>
-                        <Form.Control/>
-                    </Col>
+                <Form.Group className="account-row">
+                    <h2 className="account-label">Last name</h2>
+                    <Form.Control defaultValue="name1"/>
                 </Form.Group>
 
-                <Form.Group as={Row} className="account-row">
-                    <Form.Label className="account-label">Email</Form.Label>
-                    <Col>
-                        <Form.Control/>
-                    </Col>
+                <Form.Group className="account-row">
+                    <h2 className="account-label">Email</h2>
+                    <Form.Control defaultValue="name1 name2"/>
                 </Form.Group>
 
-                <Form.Group as={Row} className="account-row" controlId="formBasicPassword">
-                    <Form.Label className="account-label">Password</Form.Label>
-                    <Col>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Col>
+                <Form.Group className="account-row last-row">
+                    <h2 className="account-label">Date of Birth</h2>
+                    <Form.Control defaultValue="name1 name2"/>
                 </Form.Group>
 
                 <Button id="account-save-btn" className="account-row" variant="primary" type="submit">
                     Save
                 </Button>
             </Form>
-        </div>
+        </Card>
     );
 }
 
