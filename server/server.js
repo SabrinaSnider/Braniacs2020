c8700e83b3614dacdef9b7a440109074e840eeff
Sabrina Snider <sabrina.snider10@gmail.com>
2020-03-31 20:04:41 -0400

    Commented out production check

--- server/server.js
+++ server/server.js
@@ -28,14 +28,14 @@ app.use('/maps', mapsRouter)
 app.use('/patient', patientDbRouter)
 app.use('/appt', apptDbRouter)
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, '../../client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, '../../client/build')));
 
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-    });
-}
+//     // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//         res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+//     });
+// }
 
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
