bbf64d2ea08836c6295654d218091ee87ed93b02
Sabrina Snider <sabrina.snider10@gmail.com>
2020-03-31 21:32:20 -0400

    Changed some server.js stuff back to original values

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
-const express = require('express')
+const express = require('./config/express.js')
 const config = require('./config/config.js')
 const mapsRouter = require('./routes/mapsRouter')
 const patientDbRouter = require('./routes/patientDbRouter')
 const apptDbRouter = require('./routes/apptDbRouter')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const path = require('path')
  
 mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
     console.log(`Successfully connected to mongoose database.`)
@@ -15,27 +12,10 @@ mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true
 // Use env port or default
 const port = process.env.PORT || 5000;
 
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
-
-const app = express();
-// enable request logging for development debugging
-app.use(morgan('dev'));
-// body parsing middleware
-app.use(bodyParser.json());
+const app = express.init();
 
 app.use('/maps', mapsRouter)
 app.use('/patient', patientDbRouter)
 app.use('/appt', apptDbRouter)
-
-// if (process.env.NODE_ENV === 'production') {
-//     // Serve any static files
-//     app.use(express.static(path.join(__dirname, '../../client/build')));
-
-//     // Handle React routing, return all requests to React app
-//     app.get('*', function(req, res) {
-//         res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-//     });
-// }
 
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
