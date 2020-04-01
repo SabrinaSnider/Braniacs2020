eebca34b93b48c6591dd2ade6fdc5af4e332f209
Sabrina Snider <sabrina.snider10@gmail.com>
2020-03-31 20:25:26 -0400

    Deleted duplicates

--- client/src/components/Appointment/src/reducers/index 2.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import AuthReducer from './AuthReducer';
-import AppointmentsReducer from './AppointmentsReducer';
-
-export default combineReducers({
-    auth: AuthReducer,
-    appointments: AppointmentsReducer
-});
