ac15fb9378818c036556987f734f13db2f696375
ezralee3 <48745643+ezralee3@users.noreply.github.com>
2020-04-14 20:07:25 -0400

    Forgot Password stuff

---
+++ client/src/components/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import './ForgotPassword.css'
+import httpUser from '../../httpUser'
+
+const ForgotPassword = (props) => {
+    return (
+        <div class="row">
+			<div class="col-md-12">
+				<form action="/forgot" method="POST">
+					<legend>Forgot Password</legend>
+					<div class="form-group">
+						<label for="email">Email</label>
+						<input type="email" name="email" autofocus class="form-control"></input>
+					</div>
+					<div class="form-group">
+						<input type="submit" class="btn btn-primary" value="Reset Password"></input>
+					</div>
+				</form>
+			</div>
+		</div>
+    );
+}
+
+export default ForgotPassword;
