c98c3057ee351e7b93c2f5e0fe2a9f86b82434d1
ezralee3 <48745643+ezralee3@users.noreply.github.com>
2020-04-14 20:15:03 -0400

    Forgot Password stuff

--- server/routes/patientDbRouter.js
+++ server/routes/patientDbRouter.js
@@ -1,23 +1,38 @@
-const patientDb = require('../controllers/patientDbController.js')
-const config = require('../config/config.js')
-const express = require('express')
 const mongoose = require('mongoose')
-verifyToken = require('../authHelperFunctions').verifyToken;
+const bcrypt = require('bcrypt-nodejs')
 
-const Router = express.Router();
+const patientSchema = new mongoose.Schema({
+	email: {type: String, required: true},
+	password: {type: String, required: true},
+	clinicId: {type: String},
+	dob: {type: Date},
+	name: {
+		first: {type: String},
+		last: {type: String}
+	},
+	resetPasswordToken: {type: String},
+	resetPasswordExpires: {type: Date}
+});
 
-/*Patient Database Router*/
+patientSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+};
 
-Router.get('/emails', patientDb.fetchEmails); //fetch emails
-Router.get('/create', patientDb.popPatients); //testing function
-Router.get('/user', patientDb.fetchUser); //fetch user information
+// adds method to user to check if password is correct
+patientSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
-Router.post('/useraccount', patientDb.fetchUserFromEmail); //fetch user information
-Router.post('/register', patientDb.newPatient); //create user
-Router.post('/authenticate', patientDb.authenticate);
+// had to add this, checks if password was changed before saving
+patientSchema.pre('save', function(next) {
+    if(this.isModified('password')) {
+        this.password = this.generateHash(this.password);
+    }
+    next();
+});
 
-Router.put('/update', patientDb.updatePatients); //update account info function?
-
-Router.use(verifyToken);
-
-module.exports = Router
+/* Use your schema to instantiate a Mongoose model
+Export the model to make it avaiable to other parts of your Node application */
+//Check out - https://mongoosejs.com/docs/guide.html#models
+const Patient = mongoose.model('patientDB', patientSchema)
+module.exports = Patient;
