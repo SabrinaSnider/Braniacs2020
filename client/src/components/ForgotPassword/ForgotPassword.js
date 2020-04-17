194e8c18dc341b429aa027f08a092729fa067792
ezralee3 <48745643+ezralee3@users.noreply.github.com>
2020-04-16 23:00:21 -0400

    Displays messages to user

--- client/src/components/ForgotPassword/ForgotPassword.js
+++ client/src/components/ForgotPassword/ForgotPassword.js
@@ -3,14 +3,41 @@ import './ForgotPassword.css'
 import httpUser from '../../httpUser'
 
 const ForgotPassword = (props) => {
+	const [email, setEmail] = useState("");
+	const [errors, setErrors] = useState({
+        email: ""
+	});
+
+	const onEmailChange = (e) => {
+        e.persist();
+        setEmail(e.target.value);
+	};
+	
+	const onFormSubmit = async(e) => {
+		//alert('Please check your email for further instructions. If you did not receive an email, please make sure you entered your email address properly.')
+		e.preventDefault();
+		const newUser = {
+            email: email,
+        }
+		const user = await httpUser.validateEmail(newUser);
+		if(user.errors !== undefined){
+            setErrors({
+                email: user.errors.email,
+            });
+		}
+	}
+
     return (
         <div class="row">
 			<div class="col-md-12">
-				<form action="/patient/forgot" method="POST">
+				<form onSubmit={onFormSubmit}>
 					<legend>Forgot Password</legend>
 					<div class="form-group">
-						<label for="email">Email</label>
-						<input type="email" name="email" autofocus class="form-control"></input>
+						<label htmlFor="email" className="form-label">Email</label>
+						<input type="email" className="form-control" placeholder="" onChange = {onEmailChange}></input>
+						{errors.email !== undefined &&
+							<label id = "error">{errors.email}</label>
+						}
 					</div>
 					<div class="form-group">
 						<input type="submit" class="btn btn-primary" value="Reset Password"></input>
